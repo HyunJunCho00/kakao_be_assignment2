cbb69fa1e0b624aba9894a37ca8cc47726e97ec4
HyunJunCho00 <162411774+HyunJunCho00@users.noreply.github.com>
2025-05-23 14:18:47 +0900

    Update ScheduleUpdateDto.java

--- Lv1~2/src/main/java/com/example/schedule/dto/ScheduleUpdateDto.java
+++ Lv1~2/src/main/java/com/example/schedule/dto/ScheduleUpdateDto.java
@@ -1,5 +1,6 @@
 package com.example.schedule.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,5 +10,6 @@ public class ScheduleUpdateDto {
     private String title;
     private String task;
     private String author;
+    @NotBlank(message = "비밀번호는 필수입니다.")
     private String password; // 비밀번호 검증을 위해 포함
 }
